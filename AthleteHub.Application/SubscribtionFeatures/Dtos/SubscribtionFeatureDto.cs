fc4a1f096bf1ce55a23575fed0a9415d4f7384fc
Moaz <moazsamy333@gmail.com>
2024-06-23 16:13:32 +0300

    (WIP) Adding SubscribtionFeature dto

---
+++ AthleteHub.Application/SubscribtionFeatures/Dtos/SubscribtionFeatureDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AthleteHub.Application.SubscribtionFeatures.Dtos
+{
+    public class SubscribtionFeatureDto
+    {
+        public int SubscribtionId { get; set; }
+        public int FeatureId { get; set; }
+    }
+}
