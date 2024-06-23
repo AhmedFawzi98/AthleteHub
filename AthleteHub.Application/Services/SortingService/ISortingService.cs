796246e22ab0ed1f665d292bbb6fe37926ce3737
Moaz <moazsamy333@gmail.com>
2024-06-23 16:42:58 +0300

    Added SortingService for Subscribtions, SubscribtionFeatures

---
+++ AthleteHub.Application/Services/SortingService/ISortingService.cs
@@ -0,0 +1,16 @@
+﻿using AthleteHub.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AthleteHub.Application.Services.SortingService
+{
+    public interface ISortingService
+    {
+        Expression<Func<Subscribtion, object>> GetSubscribtionSortingExpression(string sortBy);
+        Expression<Func<SubscribtionFeature, object>> GetSubscribtionFeatureSortingExpression(string sortBy);
+    }
+}
---
+++ AthleteHub.Application/Services/SortingService/SortingService.cs
@@ -0,0 +1,38 @@
+﻿using AthleteHub.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AthleteHub.Application.Services.SortingService
+{
+    public class SortingService : ISortingService
+    {
+        Dictionary<string, Expression<Func<Subscribtion, object>>> subscribtionExpressionsDictionary = new()
+        {
+            { nameof(Subscribtion.Name).ToLower(), r => r.Name },
+            { nameof(Subscribtion.price), r => r.price }
+        };
+        Dictionary<string, Expression<Func<SubscribtionFeature, object>>> subscribtionFeatureExpressionDictionary = new()
+        {
+            { nameof(SubscribtionFeature.Feature.Name).ToLower(), r => r.Feature.Name
+            },
+            { nameof(SubscribtionFeature.Subscribtion.Name).ToLower(), r => r.Subscribtion.Name
+            },
+            { nameof(SubscribtionFeature.Subscribtion.price), r => r.Subscribtion.price
+            }
+        };
+
+        public Expression<Func<Subscribtion, object>> GetSubscribtionSortingExpression(string sortBy)
+        {
+            return subscribtionExpressionsDictionary[sortBy.ToLower()];
+        }
+
+        public Expression<Func<SubscribtionFeature, object>> GetSubscribtionFeatureSortingExpression(string sortBy)
+        {
+            return subscribtionFeatureExpressionDictionary[sortBy.ToLower()];
+        }
+    }
+}
