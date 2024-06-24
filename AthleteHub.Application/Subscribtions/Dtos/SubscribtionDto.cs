1aebf6e62a676b2215d390dbbdf7bb63aa7270df
Moaz <moazsamy333@gmail.com>
2024-06-24 23:52:22 +0300

    Modified CoachId field in subscribtionDTO

--- AthleteHub.Application/Subscribtions/Dtos/SubscribtionDto.cs
+++ AthleteHub.Application/Subscribtions/Dtos/SubscribtionDto.cs
@@ -11,7 +11,7 @@ namespace AthleteHub.Application.Subscribtions.Dtos
     public class SubscribtionDto
     {
         public int Id { get; set; }
-        public int CoachId { get; }
+        public int CoachId { get; set; }
         public string Name { get; set; }
         public decimal price { get; set; }
         public int DurationInMonths { get; set; }
