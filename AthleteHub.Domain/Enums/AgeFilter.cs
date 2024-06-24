579795cc8bf7fba859d65614bbd49184ccebc118
MostafaAymanAwad <ayelshekh@gmail.com>
2024-06-24 23:10:10 +0300

    Adding Sorting Enums and Update the Genric repositary Interface

---
+++ AthleteHub.Domain/Enums/AgeFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AthleteHub.Domain.Enums
+{
+    public enum AgeFilter
+    {
+        lessThan20,
+        between20and25,
+        between25and30,
+        moeThan30
+    }
+}
