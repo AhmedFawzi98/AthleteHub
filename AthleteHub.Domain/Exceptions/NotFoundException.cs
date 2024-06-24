c7333f83422a94a3cf573c9f36c3aad64db8d043
Moaz <moazsamy333@gmail.com>
2024-06-24 23:51:54 +0300

    Added NotFoundException class

---
+++ AthleteHub.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AthleteHub.Domain.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string resourceType, string resourceId):base($"{resourceType} [{resourceId}] doesn't exist") 
+        {
+        }
+    }
+}
