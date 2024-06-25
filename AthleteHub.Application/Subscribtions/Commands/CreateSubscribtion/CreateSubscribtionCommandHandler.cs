2da5f16ef272ef2e36bcaae1a6967c86a845fe23
Moaz <moazsamy333@gmail.com>
2024-06-25 17:03:59 +0300

    No need for checking coach as it is done in controller

--- AthleteHub.Application/Subscribtions/Commands/CreateSubscribtion/CreateSubscribtionCommandHandler.cs
+++ AthleteHub.Application/Subscribtions/Commands/CreateSubscribtion/CreateSubscribtionCommandHandler.cs
@@ -1,9 +1,11 @@
 ﻿using AthleteHub.Application.Subscribtions.Dtos;
 using AthleteHub.Application.Users;
+using AthleteHub.Domain.Constants;
 using AthleteHub.Domain.Entities;
 using AthleteHub.Domain.Interfaces.Repositories;
 using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -31,25 +33,20 @@ namespace AthleteHub.Application.Subscribtions.Commands.CreateSubscribtion
         public async Task<SubscribtionDto> Handle(CreateSubscribtionCommand request, CancellationToken cancellationToken)
         {
             var currentUser = _userContext.GetCurrentUser();
-            if (currentUser.IsInRole("Coach"))
-            {
-                int coachId;
-                int.TryParse(currentUser.Id, out coachId);
+            
+            int coachId;
+            int.TryParse(currentUser.Id, out coachId);
 
-                var newSubscribtion = _mapper.Map<Subscribtion>(request);
-                newSubscribtion.CoachId = coachId;
+            var newSubscribtion = _mapper.Map<Subscribtion>(request);
+            newSubscribtion.CoachId = coachId;
 
-                await _unitOfWork.Subscribtions.AddAsync(newSubscribtion);
-                await _unitOfWork.CommitAsync();
+            await _unitOfWork.Subscribtions.AddAsync(newSubscribtion);
+            await _unitOfWork.CommitAsync();
 
-                var addedSubscribtion = _mapper.Map<SubscribtionDto>(newSubscribtion);
-                _logger.LogInformation("CreateSubscribtionCommand was invoked by {username} with id = {userid}, Created restaurant: {@newSubscribtion}", currentUser.Name, currentUser.Id, newSubscribtion);
+            var addedSubscribtion = _mapper.Map<SubscribtionDto>(newSubscribtion);
+            _logger.LogInformation("CreateSubscribtionCommand was invoked by {username} with id = {userid}, Created restaurant: {@newSubscribtion}", currentUser.Name, currentUser.Id, newSubscribtion);
 
-                return addedSubscribtion;
-            }
-            _logger.LogInformation("CreateSubscribtionCommand was invoked by {username} with id = {userid}, who is not a Coach, invokation halted and subscribtion was not created.", currentUser.Name, currentUser.Id);
-
-            return new SubscribtionDto();
+            return addedSubscribtion;
         }
     }
 }
